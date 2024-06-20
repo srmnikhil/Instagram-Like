c962f0c8e3916506ddb37ebf6e9cac4357542986
srmnikhil <srmnikhilswn@gmail.com>
2024-06-21 00:42:39 +0530

    Updated

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 var like = document.querySelector("#container");
 var heart = document.querySelector("i");
-
 like.addEventListener("dblclick", function(){
-    heart.style.transform = "translate(-50%, -50%) scale(0)"
-    heart.style.opacity = "1"
-    setTimeout(function() {
-        heart.style.transform = "translate(-50%, -50%) scale(1)";
-    }, 0);
+    heart.style.transform = "translate(-50%, -50%) scale(1)";
+    heart.style.opacity="0.8"
     setTimeout(function(){
         heart.style.opacity = "0"
     }, 1000)
+    setTimeout(function(){
+        heart.style.transform = ""
+    }, 1100)
 })
